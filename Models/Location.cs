242036ed6cddfa2ac161af455cd066b265e61735
Lucas Lumertz <lumertzdeveloper@hotmail.com>
2023-11-14 22:31:58 -0300

    update model

--- Models/Location.cs
+++ Models/Location.cs
@@ -9,24 +9,17 @@ public class Location
     /// Código de 7 digitos do IBGE
     /// </summary>
     /// <example>1234567</example>
-    [Key]
-    [Column(TypeName = "varchar")]
-    [StringLength(7)]
     public string Id { get; set; }
 
     /// <summary>
     /// Sigla do Estado
     /// </summary>
     /// <example>SC</example>
-    [Column(TypeName = "varchar")]
-    [StringLength(2)]
     public string State { get; set; } = string.Empty;
 
     /// <summary>
     /// Nome da cidade
     /// </summary>
     /// <example>Florianópolis</example>
-    [Column(TypeName = "varchar")]
-    [StringLength(90)]
     public string City { get; set; } = string.Empty;
 }
